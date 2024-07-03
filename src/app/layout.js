bf5d9a0f6cf69b6dd4351343ea17c0fc3f81cf0d
404reese <riddhesh.chaudhary@gmail.com>
2024-07-03 21:42:21 +0530

    upload-route
    Co-Authored-By: d3fa4lt <174237821+d3fa4lt@users.noreply.github.com>

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "../components/navbar/Navbar"
+import Footer from "../components/footer/Footer"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: {
+    default:"Class Connect",
+    template:"Welcome"
+  },
+  description: "Welcome to class connect",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <ClientSideProviderTest> */}
+          <div className="container">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        {/* </ClientSideProviderTest> */}
+      </body>
+    </html>
+  );
+}
