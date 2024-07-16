b9f7644b6bc5d4180908d81799cd5f7dc470bc8c
404reese <riddhesh.chaudhary@gmail.com>
2024-07-16 18:09:42 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/navbar/Navbar"
-import Footer from "../components/footer/Footer"
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default:"Class Connect",
-    template:"Welcome"
+    default: "Class Connect",
+    template: "Welcome",
   },
   description: "Welcome to class connect",
 };
@@ -17,14 +18,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Analytics />
+      <head>
+        <Analytics />
+
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+      page_path: window.location.pathname,
+    });
+        `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {/* <ClientSideProviderTest> */}
-          <div className="container">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
+        <div className="container">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
         {/* </ClientSideProviderTest> */}
       </body>
     </html>
