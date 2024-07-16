c0bc9024e07a468e6fba13818e6985e1ae0e7e9e
404reese <riddhesh.chaudhary@gmail.com>
2024-07-16 16:43:16 +0530

    ver-ana-react

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar"
 import Footer from "../components/footer/Footer"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className={inter.className}>
         {/* <ClientSideProviderTest> */}
           <div className="container">
