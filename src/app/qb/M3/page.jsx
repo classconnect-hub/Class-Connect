6a7548939e2dbc1c49b64ffd5ae130af209fa6c5
404reese <riddhesh.chaudhary@gmail.com>
2024-08-23 21:13:00 +0530

    Update page.jsx

--- src/app/qb/M3/page.jsx
+++ src/app/qb/M3/page.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const DSDS = () => {
   return (
     <div style={{ width: '100%', overflow: 'hidden' }}>
+      <a href='https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EZfUQiSsZ8lLrI65pp4iSGgBDYsMolspoSbshpfySquv8w?e=PbcjtG'><h3>Maths 3 Kumbhojkar</h3></a>
         <h1 style={{marginBottom: '20px'}}>Practice questions my Prajakta mam</h1>
         <iframe
       src="https://docs.google.com/document/d/1EY4skhHYHTAEOHnC1AD34MicelJ1CnL0NU2ZwHNSsBs/pub?embedded=true"
@@ -11,7 +12,8 @@ const DSDS = () => {
     ></iframe>
       <h1 style={{marginBottom: '20px', marginTop: '20px'}}>Module 1 - Laplace transform</h1>
       <iframe src="https://somaiya0-my.sharepoint.com/personal/riddhesh_c_somaiya_edu/_layouts/15/embed.aspx?UniqueId=1ad27d48-13a4-4baa-b514-039f9fbd84f2" width="100%" height="600" frameborder="0" scrolling="no" allowfullscreen title="Laplace transform (Prajakta ma’am notes)"></iframe>
-
+      <h1 style={{marginBottom: '20px', marginTop: '20px'}}>Module 2 (few from M1)</h1>
+      <iframe src="https://somaiya0-my.sharepoint.com/personal/riddhesh_c_somaiya_edu/_layouts/15/embed.aspx?UniqueId=fa8ddd60-dae6-4e10-b7cf-16a8ad3d56e2" width="100%" height="600" frameborder="0" scrolling="no" allowfullscreen title="Laplace 1,2 "></iframe>
       </div>
   );
 };
