d9fe4c5cecc59d722ee728ba8e44ee696396342f
404reese <riddhesh.chaudhary@gmail.com>
2024-08-23 18:58:14 +0530

    Update page.jsx

--- src/app/qb/M3/page.jsx
+++ src/app/qb/M3/page.jsx
@@ -4,9 +4,13 @@ const DSDS = () => {
   return (
     <div style={{ width: '100%', overflow: 'hidden' }}>
         <h1 style={{marginBottom: '20px'}}>Practice questions my Prajakta mam</h1>
-      <iframe src="https://somaiya0-my.sharepoint.com/personal/riddhesh_c_somaiya_edu/_layouts/15/embed.aspx?UniqueId=f87d96cd-eaca-4e90-98d5-1646ac7ec16e" width="100%" height="500" frameborder="0" scrolling="no" allowfullscreen title="Practice questions Module-1,2,4"></iframe>
+        <iframe
+      src="https://docs.google.com/document/d/1EY4skhHYHTAEOHnC1AD34MicelJ1CnL0NU2ZwHNSsBs/pub?embedded=true"
+      style={{ width: '100%', border: 'none', height: '600px' }}
+      title="Embedded Document"
+    ></iframe>
       <h1 style={{marginBottom: '20px', marginTop: '20px'}}>Module 1 - Laplace transform</h1>
-      <iframe src="https://somaiya0-my.sharepoint.com/personal/riddhesh_c_somaiya_edu/_layouts/15/embed.aspx?UniqueId=1ad27d48-13a4-4baa-b514-039f9fbd84f2" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen title="Laplace transform"></iframe>
+      <iframe src="https://somaiya0-my.sharepoint.com/personal/riddhesh_c_somaiya_edu/_layouts/15/embed.aspx?UniqueId=1ad27d48-13a4-4baa-b514-039f9fbd84f2" width="100%" height="600" frameborder="0" scrolling="no" allowfullscreen title="Laplace transform (Prajakta ma’am notes)"></iframe>
 
       </div>
   );
