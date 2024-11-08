9d8d6f869d37a300c62c9c694ebc0b98315e22ca
Riddhesh <riddhesh.chaudhary@gmail.com>
2024-11-08 19:39:19 +0530

    Update pData.js

--- src/app/pyq/data/pData.js
+++ src/app/pyq/data/pData.js
@@ -34,13 +34,53 @@ const pData = [
     {
         "id": 5,
         "department": "FY",
+        "semester": "Sem1",
+        "subject": "EM",
+        "note": "EM TT1 2023-24",
+        "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EdSNf0Rr9wxBiZK8daVGS6YBMlnWHRqirO9curdxIlb4Gg?e=MIJ09U"
+    },
+    {
+        "id": 6,
+        "department": "FY",
+        "semester": "Sem1",
+        "subject": "EM1",
+        "note": "EM1 TT1 2023-24",
+        "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/Ee4VPUZnLGxLqEfbHHbB4IMBpBN81IcyMNTAzgQiNZfXlg?e=IGvBR4"
+    },
+    {
+        "id": 7,
+        "department": "FY",
+        "semester": "Sem1",
+        "subject": "EP",
+        "note": "EP TT1 2023-24",
+        "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EQNUQbCA7VFDnAPdsf5h0SEBsevSiww0bwpXRxIfIZdZsA?e=UbqbTw"
+    },
+    {
+        "id": 8,
+        "department": "FY",
+        "semester": "Sem1",
+        "subject": "EC",
+        "note": "EC TT1 2023-24",
+        "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/Ecp8EVv1PXBBkkYI1rDsPyQBx9XiGZq_ClgZRVv1qrbqWw?e=ZnltjC"
+    },
+    {
+        "id": 9,
+        "department": "FY",
+        "semester": "Sem1",
+        "subject": "BEE",
+        "note": "BEE TT1 2023-24",
+        "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EfFNpPzaj1pBsR-8iTfIO1UBJwoT0m2d6T4aehzPcRVeNQ?e=8aarl5"
+    },
+    {
+        "id": 10,
+        "department": "FY",
         "semester": "Sem2",
         "subject": "Chem 2",
         "note": "sem 2 ESE chemistry",
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ER2PYmz_VH5Er3hJpD0UJKcBNRnEsQzUQAzxc8qSS_APoA?e=7wdQPL"
     },
     {
-        "id": 6,
+        "id": 11,
         "department": "FY",
         "semester": "Sem2",
         "subject": "CP",
@@ -48,7 +88,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ERpR-H4OPjpNgRJKbj3-OLQBuOe5JIsk9cXPP1bA88baRA?e=C91PTU"
     },
     {
-        "id": 7,
+        "id": 12,
         "department": "FY",
         "semester": "Sem2",
         "subject": "EG",
@@ -56,7 +96,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ETMU-U_Aw3lHkItaKHxLxCgB9ifJWKRmIeVxy6Xa14wphw?e=MQDhyK"
     },
     {
-        "id": 8,
+        "id": 13,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Maths 2",
@@ -64,7 +104,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ETnaY4amqKNLk9pbUG0Jxu4BBF6RUuQqsiL2L1FTdSecww?e=hZHAdv"
     },
     {
-        "id": 9,
+        "id": 14,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Phy 2",
@@ -72,7 +112,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/Ec94hTX4sK9BgxY8vZqJDbUBU_4bEG6PJVJHJ_wRCJqrXg?e=1uLiya"
     },
     {
-        "id": 10,
+        "id": 15,
         "department": "FY",
         "semester": "Sem2",
         "subject": "All",
@@ -80,7 +120,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EfBaolvw_5BGidBcGLQ8E0YBWpAYoyEtYKOTvNai4MOOCQ?e=94EbbB"
     },
     {
-        "id": 11,
+        "id": 16,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Chem 2",
@@ -88,7 +128,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EdSEvTbfkbBLmwxdU-CGmhEBcuhxDz7m2MITV3sHWMffew?e=Ro7Nz6"
     },
     {
-        "id": 12,
+        "id": 17,
         "department": "FY",
         "semester": "Sem2",
         "subject": "CP",
@@ -96,7 +136,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ES84YlvSghRKg2LPg0bNvj4B0uxYLrFYrdSOZfcNxcHMew?e=sA1pr6"
     },
     {
-        "id": 13,
+        "id": 18,
         "department": "FY",
         "semester": "Sem2",
         "subject": "EG",
@@ -104,7 +144,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/ES_wGl85WLRDkvvMQaf45JkB0xKq4OMArtyhZg4b22E71g?e=IwejtK"
     },
     {
-        "id": 14,
+        "id": 19,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Maths 2",
@@ -112,7 +152,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EWYk_nOMJgZNnysJwp1xjDkBeDhcKvnPBYbRhlXuiK43kg?e=klzZDG"
     },
     {
-        "id": 15,
+        "id": 20,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Phy 2",
@@ -120,7 +160,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:b:/g/personal/riddhesh_c_somaiya_edu/EbuWbKMVYllBtPBPCeVP8AsBL69mRWINogX2pTatPBSPsA?e=vlOgsk"
     },
     {
-        "id": 16,
+        "id": 21,
         "department": "FY",
         "semester": "Sem2",
         "subject": "CP",
@@ -128,7 +168,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:f:/g/personal/riddhesh_c_somaiya_edu/EkL2Q3Pc71lIuVL8-0MDfokBFPrE3Hxay8pUI9Z0ki8ZdA?e=VSihLh"
     },
     {
-        "id": 17,
+        "id": 22,
         "department": "FY",
         "semester": "Sem2",
         "subject": "EG",
@@ -136,7 +176,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:f:/g/personal/riddhesh_c_somaiya_edu/EuV9omP0DbFCsJ29A4fUdNAB0KO8RZY6DO8tG4Af9hbZZQ?e=esFqMS"
     },
     {
-        "id": 18,
+        "id": 23,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Maths 2",
@@ -144,7 +184,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:f:/g/personal/riddhesh_c_somaiya_edu/Eulw2eTT_8lPpSjY_ccIC_YBkkluwHYlUtqbt3l2qM1Dgw?e=YkRHVt"
     },
     {
-        "id": 19,
+        "id": 24,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Chem 2",
@@ -152,7 +192,7 @@ const pData = [
         "link": "https://somaiya0-my.sharepoint.com/:f:/g/personal/riddhesh_c_somaiya_edu/EhB7jTUg2-1OvimO677RyWgBNe9d4BPiS3NX0-auKskd0A?e=yQfLYT"
     },
     {
-        "id": 20,
+        "id": 25,
         "department": "FY",
         "semester": "Sem2",
         "subject": "Phy 2",
